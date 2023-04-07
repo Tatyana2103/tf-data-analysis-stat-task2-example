a2584b0cc968e08a5d2ec0e5805204af2d3414ba
Tatyana2103 <71606262+Tatyana2103@users.noreply.github.com>
2023-04-07 22:20:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,11 @@
+
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import gamma
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 956745134 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
@@ -13,5 +14,5 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     loc = x.mean()
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    return (loc - len(x)/2 + gamma.ppf(x = alpha/2, a = len(x) , loc=0, scale=1))/(len(x)*43*86), \
+           (loc - len(x)/2 + gamma.ppf(x =1- alpha/2, a = len(x) , loc=0, scale=1))/(len(x)*43*86)
